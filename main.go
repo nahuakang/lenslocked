9f75fb580ab69aaca05197ed41fe40eca0b22186
Nahua <kangnahua@gmail.com>
2022-03-14 23:41:56 +0100

    Add air

--- main.go
+++ main.go
@@ -6,11 +6,11 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	fmt.Fprint(w, "<h1>Welcome to my new site!</h1>")
 }
 
 func main() {
-    http.HandleFunc("/", handlerFunc)
-    fmt.Println("Starting the server on :3000")
-    http.ListenAndServe(":3000", nil)
+	http.HandleFunc("/", handlerFunc)
+	fmt.Println("Starting the server on :3000")
+	http.ListenAndServe(":3000", nil)
 }
