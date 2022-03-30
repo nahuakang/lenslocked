7eab6971e25a9614741e8b0fac769ec9ea1952b6
Nahua <kangnahua@gmail.com>
2022-03-30 10:35:35 +0200

    Custom path router

--- main.go
+++ main.go
@@ -5,12 +5,31 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to my new site!</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:nahua@lenselocked.com\">nahua@lenselocked.com</a>.</p>")
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "Page not found")
+	}
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", pathHandler)
+
 	fmt.Println("Starting the server on :3000")
 	http.ListenAndServe(":3000", nil)
 }
