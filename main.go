f642155d5c6bbd6135b0d59b3e3e0303fb27c6c6
Nahua <kangnahua@gmail.com>
2022-03-30 11:41:23 +0200

    Use chi for routing

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 type Router struct{}
@@ -29,8 +31,36 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:nahua@lenselocked.com\">nahua@lenselocked.com</a>.</p>")
 }
 
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>FAQ Page</h1>
+<ul>
+  <li>
+    <b>Is there a free version?</b>
+    Yes! We offer a free trial for 30 days on any paid plans.
+  </li>
+  <li>
+    <b>What are your support hours?</b>
+    We have support staff answering emails 24/7, though response
+    times may be a bit slower on weekends.
+  </li>
+  <li>
+    <b>How do I contact support?</b>
+    Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>
+  </li>
+</ul>
+`)
+}
+
 func main() {
-	var router Router
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
+
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":3000", r)
 }
