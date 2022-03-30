2bee004dac4cc7de37b528c349bb0b3ec04f3a6c
Nahua <kangnahua@gmail.com>
2022-03-30 11:25:34 +0200

    Use a router that implements serve http

--- main.go
+++ main.go
@@ -5,17 +5,9 @@ import (
 	"net/http"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Welcome to my new site!</h1>")
-}
+type Router struct{}
 
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:nahua@lenselocked.com\">nahua@lenselocked.com</a>.</p>")
-}
-
-func pathHandler(w http.ResponseWriter, r *http.Request) {
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
@@ -27,9 +19,18 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func main() {
-	http.HandleFunc("/", pathHandler)
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Welcome to my new site!</h1>")
+}
 
-	fmt.Println("Starting the server on :3000")
-	http.ListenAndServe(":3000", nil)
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:nahua@lenselocked.com\">nahua@lenselocked.com</a>.</p>")
+}
+
+func main() {
+	var router Router
+	fmt.Println("Starting the server on :3000...")
+	http.ListenAndServe(":3000", router)
 }
