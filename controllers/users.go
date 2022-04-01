9231390852e0cbce2cbc036654270e6bc98398d9
Nahua <kangnahua@gmail.com>
2022-04-01 09:22:43 +0200

    Parse signup form; use query param for autofocus

--- controllers/users.go
+++ controllers/users.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -11,5 +12,24 @@ type Users struct {
 }
 
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
-	u.Templates.New.Execute(w, nil)
+	var data struct {
+		Email string
+	}
+	data.Email = r.FormValue("email")
+	u.Templates.New.Execute(w, data)
+}
+
+func (u Users) Create(w http.ResponseWriter, r *http.Request) {
+	// err := r.ParseForm()
+	// if err != nil {
+	// 	http.Error(w, "Unable to parse form submission.", http.StatusBadRequest)
+	// 	return
+	// }
+	// fmt.Fprintf(w, "<p>Email: %s</p>", r.PostForm.Get("email"))
+	// fmt.Fprintf(w, "<p>Password: %s</p>", r.PostForm.Get("password"))
+
+	// Instead of r.PostForm.Get, use r.FormValue works, too
+	fmt.Fprintf(w, "<p>Email: %s</p>", r.FormValue("email"))
+	fmt.Fprintf(w, "<p>Password: %s</p>", r.FormValue("password"))
+
 }
--- main.go
+++ main.go
@@ -13,25 +13,25 @@ import (
 func main() {
 	r := chi.NewRouter()
 
+	// Static pages
 	r.Get("/", controllers.StaticHandler(views.Must(
 		views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))))
-
 	r.Get("/contact", controllers.StaticHandler(views.Must(
 		views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))))
-
 	r.Get("/faq", controllers.FAQ(
 		views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
-
 	r.Get("/signup", controllers.StaticHandler(
 		views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))))
-
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
 
+	// Users controller
 	var usersC controllers.Users
-	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
+	usersC.Templates.New = views.Must(
+		views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
 	r.Get("/signup", usersC.New)
+	r.Post("/signup", usersC.Create)
 
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", r)
